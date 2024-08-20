ea0ba555c0588ee5bc9ec9cd0f29d8fbbad47f02
Dulce Fabiola <dulcefabiola.rm@gmail.com>
2024-08-20 13:30:38 -0600

    feature/product details view

--- src/Router.tsx
+++ src/Router.tsx
@@ -3,6 +3,7 @@ import { ChakraProvider, Container } from "@chakra-ui/react";
 import PrimaryLayout from "./components/layout";
 import HomePage from "./pages/home";
 import LoginPage from "./pages/login";
+import ProductDetailsPage from "./pages/productDetails";
 import theme from "../src/theme";
 import "../src/components/layout/index.css";
 const Router = () => {
@@ -13,6 +14,7 @@ const Router = () => {
           <Routes>
             <Route path="/" element={<PrimaryLayout />}>
               <Route index element={<HomePage />} />
+              <Route path="/producto/:id" element={<ProductDetailsPage />} />
               {/* Rutas de Usuario*/}
               <Route path="inicio-sesion" element={<LoginPage />} />
             </Route>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import HomeBanner from "../../components/homeBanner/banner";
 import ProductList from "../../components/products/productList";
-import "./index.css";
+
 const HomePage = () => {
   return (
     <>
